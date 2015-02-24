510a56871914c9cc522e1bc745a3c28a6527e9fe
Sergi Pscherer <sergipscherer@MacBook-MacBook-Pro-de-Sergi.local>
2015-02-24 16:19:06 +0100

    Registre Validat

--- MW_clientSwift/Controllers/VCRegister.swift
+++ MW_clientSwift/Controllers/VCRegister.swift
@@ -27,7 +27,11 @@ class VCRegister: UIViewController {
 
     @IBAction func registerTapped(sender: UIButton) {
         //Validar camps i enviar al servidor 
-        if(true){
+        let validarMail: Bool = application.isValidEmail(txtMail.text)
+        let validarPassword: Bool = application.isValidPassword(txtPassword.text)
+        let validarUsuari: Bool = application.validateUserName(txtUserName.text)
+        
+        if(validarMail && validarPassword && validarUsuari && txtPassword == txtConfirmPassword){
             self.performSegueWithIdentifier("goto_register_player", sender: self)
         }
     }
